ac7342288e8144d30b106198df792d7ef1c78bf4
Amit <baghlaamit06@gmail.com>
2024-11-16 18:40:13 +0530

    add feature and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
-import React from 'react';
-import Hero from './components/Hero';
-import Highlights from './components/highlights';
 import Navbar from './components/Navbar';
-import Model from './components/Model'
+import Hero from './components/Hero';
+import Highlights from './components/Highlights';
+import Model from './components/Model';
+import Features from './components/Features';
+import HowItWorks from './components/HowItWorks';
+import Footer from './components/Footer';
+
+
 const App = () => {
-  return <main className='bg-black'>
-  <Navbar/>
-  <Hero/>
-  <Highlights/>
-  <Model/>
-  </main>;
-};
+  return (
+    <main className="bg-black">
+      <Navbar />
+      <Hero />
+      <Highlights />
+      <Model />
+      <Features />
+      <HowItWorks />
+      <Footer />
+    </main>
+  )
+}
 
 export default App;
